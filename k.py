bb046f8e5a7779213c021a2bbeaed6391a496f98
cemenkawhopper <deadinside0088@gmail.com>
2024-09-24 09:23:44 +0700

    first upload
    just training

---
+++ k.py
@@ -0,0 +1,18 @@
+q1 = input('кормит детей молоком?')
+
+if q1 == 'да':
+    q2 = input('ест мясо?')
+elif q1 == 'нет':
+    q3 = input('имеет перья?')
+if q2 == 'да':
+    ans=input('хищник')
+elif q2 == 'нет':
+   ans1=input('?')
+if q3== 'да':
+    ans2=input('птица')
+elif q3 == 'нет':
+    q4=input('дышит жабрами?')
+if q4 == 'да':
+    ans3=input('рыба')
+elif q4 == 'нет':
+    ans4=input('?')
